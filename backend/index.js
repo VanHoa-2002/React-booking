1f7339886f2507435105abc178db2708f3c13b23
VNRESOURCE\hoa.tranvan <hoa.tranvan@vnresource.org>
2024-12-30 11:38:33 +0700

    feat: enhance CORS configuration and add Vercel deployment settings

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOptions = {
-  origin: true,
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  credentials: true, // Allow credentials like cookies if needed
 };
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
@@ -32,6 +35,12 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/doctors", doctorRouter);
@@ -39,7 +48,9 @@ app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/bookings", bookingRouter);
 app.use("/api/v1/contact", mailRouter);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   connectDB();
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
